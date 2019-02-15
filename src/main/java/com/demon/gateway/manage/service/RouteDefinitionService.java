5a6084b51fab11d0450442bd11916d8b7efcf605
范俊翔 <fanjx@jinyundt.com>
2019-02-15 13:10:29 +0800

    init

--- src/main/java/com/demon/gateway/manage/service/RouteDefinitionService.java
+++ src/main/java/com/demon/gateway/manage/service/RouteDefinitionService.java
@@ -26,86 +26,85 @@ import reactor.core.publisher.Mono;
 @Service
 public class RouteDefinitionService {
 
-    private final ReactiveMongoTemplate reactiveMongoTemplate;
-    private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
+  private final ReactiveMongoTemplate reactiveMongoTemplate;
+  private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
-    @Autowired
-    public RouteDefinitionService(
-        ReactiveMongoTemplate reactiveMongoTemplate,
-        ReactiveRedisTemplate<String, String> reactiveRedisTemplate) {
-        this.reactiveMongoTemplate = reactiveMongoTemplate;
-        this.reactiveRedisTemplate = reactiveRedisTemplate;
-    }
+  @Autowired
+  public RouteDefinitionService(
+      ReactiveMongoTemplate reactiveMongoTemplate,
+      ReactiveRedisTemplate<String, String> reactiveRedisTemplate) {
+    this.reactiveMongoTemplate = reactiveMongoTemplate;
+    this.reactiveRedisTemplate = reactiveRedisTemplate;
+  }
 
-    /**
-     * 获取路由列表
-     *
-     * @param page 页码
-     * @param size 每页显示数量
-     * @return 路由列表
-     */
-    public Flux<RouteDefinition> getRouteList(Integer page, Integer size) {
+  /**
+   * 获取路由列表
+   *
+   * @param page 页码
+   * @param size 每页显示数量
+   * @return 路由列表
+   */
+  public Flux<RouteDefinition> getRouteList(Integer page, Integer size) {
 
-        return reactiveMongoTemplate.find(
-            Query.query(Criteria.byExample(RouteDefinition.builder().deleted(false).build()))
-                .with(
-                    PageRequest
-                        .of(page, size, Sort.by(Sort.Order.desc(RouteDefinition.UPDATE_TIME)))),
-            RouteDefinition.class);
-    }
+    return reactiveMongoTemplate.find(
+        Query.query(Criteria.byExample(RouteDefinition.builder().deleted(false).build()))
+            .with(
+                PageRequest.of(page, size, Sort.by(Sort.Order.desc(RouteDefinition.UPDATE_TIME)))),
+        RouteDefinition.class);
+  }
 
-    /**
-     * 创建路由
-     */
-    public Mono<RouteDefinition> saveRoute(RouteDefinition routeDefinition) {
-        routeDefinition.setId(GenerateUniqueIdUtils.generateUniqueId());
+  /**
+   * 创建路由
+   */
+  public Mono<RouteDefinition> saveRoute(RouteDefinition routeDefinition) {
+    routeDefinition.setId(GenerateUniqueIdUtils.generateUniqueId());
 
-        return reactiveMongoTemplate
-            .save(routeDefinition)
-            .doOnNext(
-                rout ->
-                    reactiveRedisTemplate
-                        .opsForHash()
-                        .put(Constants.ROUTE_CACHE_KEY, rout.getId(), JsonUtils.toJsonString(rout))
-                        .subscribe());
-    }
+    return reactiveMongoTemplate
+        .save(routeDefinition)
+        .doOnNext(
+            rout ->
+                reactiveRedisTemplate
+                    .opsForHash()
+                    .put(Constants.ROUTE_CACHE_KEY, rout.getId(), JsonUtils.toJsonString(rout))
+                    .subscribe());
+  }
 
-    /**
-     * 修改路由
-     */
-    public Mono<Boolean> updateRoute(String id, RouteDefinition routeDefinition) {
+  /**
+   * 修改路由
+   */
+  public Mono<Boolean> updateRoute(String id, RouteDefinition routeDefinition) {
 
-        return reactiveMongoTemplate
-            .findAndModify(
-                Query.query(
-                    Criteria.byExample(RouteDefinition.builder().id(id).deleted(false).build())),
-                MongoUtils.buildUpdate(routeDefinition),
-                RouteDefinition.class)
-            .doOnNext(
-                rout ->
-                    reactiveRedisTemplate
-                        .opsForHash()
-                        .put(Constants.ROUTE_CACHE_KEY, rout.getId(), JsonUtils.toJsonString(rout))
-                        .subscribe())
-            .hasElement();
-    }
+    return reactiveMongoTemplate
+        .findAndModify(
+            Query.query(
+                Criteria.byExample(RouteDefinition.builder().id(id).deleted(false).build())),
+            MongoUtils.buildUpdate(routeDefinition),
+            RouteDefinition.class)
+        .doOnNext(
+            rout ->
+                reactiveRedisTemplate
+                    .opsForHash()
+                    .put(Constants.ROUTE_CACHE_KEY, rout.getId(), JsonUtils.toJsonString(rout))
+                    .subscribe())
+        .hasElement();
+  }
 
-    /**
-     * 删除路由
-     */
-    public Mono<Boolean> deleteRoute(String id) {
+  /**
+   * 删除路由
+   */
+  public Mono<Boolean> deleteRoute(String id) {
 
-        return reactiveMongoTemplate
-            .findAndModify(
-                Query.query(Criteria.byExample(RouteDefinition.builder().id(id).build())),
-                MongoUtils.buildUpdate(RouteDefinition.builder().id(id).deleted(true).build()),
-                RouteDefinition.class)
-            .doOnNext(
-                rout ->
-                    reactiveRedisTemplate
-                        .opsForHash()
-                        .remove(Constants.ROUTE_CACHE_KEY, rout.getId())
-                        .subscribe())
-            .hasElement();
-    }
+    return reactiveMongoTemplate
+        .findAndModify(
+            Query.query(Criteria.byExample(RouteDefinition.builder().id(id).build())),
+            MongoUtils.buildUpdate(RouteDefinition.builder().id(id).deleted(true).build()),
+            RouteDefinition.class)
+        .doOnNext(
+            rout ->
+                reactiveRedisTemplate
+                    .opsForHash()
+                    .remove(Constants.ROUTE_CACHE_KEY, rout.getId())
+                    .subscribe())
+        .hasElement();
+  }
 }
